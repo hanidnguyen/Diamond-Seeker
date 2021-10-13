53767d8f5754663796706ea4dbd06288b6a6cf1c
hanin <hanin@sfu.ca>
2021-10-12 22:22:03 -0700

    - Rename MainActivity to WelcomeActivity

--- app/src/main/java/cmpt276/assignment3/diamondseeker/WelcomeActivity.java
+++ app/src/main/java/cmpt276/assignment3/diamondseeker/WelcomeActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
