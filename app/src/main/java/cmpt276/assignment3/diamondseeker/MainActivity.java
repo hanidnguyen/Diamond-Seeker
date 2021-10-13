9362ce5acbe3e866ace2bb1c6f7653b8f8553516
hanin <hanin@sfu.ca>
2021-10-12 22:21:02 -0700

    - Added new activity: main_menu

--- app/src/main/java/cmpt276/assignment3/diamondseeker/MainActivity.java
+++ app/src/main/java/cmpt276/assignment3/diamondseeker/MainActivity.java
@@ -3,6 +3,8 @@ package cmpt276.assignment3.diamondseeker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setMainMenuButton();
+    }
+
+    private void setMainMenuButton() {
+        Button btn = findViewById(R.id.main_menu_btn);
+        btn.setOnClickListener(view -> {
+
+        });
     }
 }
