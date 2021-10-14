f71d11560c0555756c36ef323158abd570a7d41e
hanin <hanin@sfu.ca>
2021-10-14 01:54:13 -0700

    - Added button grids to game activity - Improved visuals in help activity

--- app/src/main/java/cmpt276/assignment3/diamondseeker/PlayGame.java
+++ app/src/main/java/cmpt276/assignment3/diamondseeker/PlayGame.java
@@ -4,19 +4,62 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 public class PlayGame extends AppCompatActivity {
+
+    private static final int NUM_ROWS = 2;
+    private static final int NUM_COLS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
-        setSupportActionBar(findViewById(R.id.game_toolbar));
-        ActionBar ab = getSupportActionBar();
-        assert ab != null;
-        ab.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.orange, getTheme())));
+        populateTableOfButtons();
+    }
+
+
+    private void populateTableOfButtons() {
+        TableLayout table = findViewById(R.id.tableForButtons);
+
+        for(int row = 0; row < NUM_ROWS;row++){
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            ));
+            table.addView(tableRow);
+
+            for(int col = 0; col < NUM_COLS;col++){
+                final int FINAL_COL = col;
+                final int FINAL_ROW = row;
+                Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+                button.setText(col + "," + row);
+
+                //Make text not clip on small buttons
+                button.setPadding(0,0,0,0);
+                button.setOnClickListener(view -> gridButtonClicked(FINAL_COL, FINAL_ROW));
+                tableRow.addView(button);
+            }
+        }
+    }
+
+    private void gridButtonClicked(int x, int y) {
+        Toast.makeText(this,"button clicked: " + x + "," + y,Toast.LENGTH_SHORT).show();
     }
 
     @Override
