aa031201f90eb54c4762e476f8bc813f0fe93d93
hanin <hanin@sfu.ca>
2021-10-15 01:22:59 -0700

    - Added welcome screen animation and icon

--- app/src/main/java/cmpt276/assignment3/diamondseeker/WelcomeActivity.java
+++ app/src/main/java/cmpt276/assignment3/diamondseeker/WelcomeActivity.java
@@ -3,15 +3,42 @@ package cmpt276.assignment3.diamondseeker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Animation topAnim;
+    ImageView image;
+    TextView title;
+    TextView author;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+        image = findViewById(R.id.welcome_diamond_icon);
+        title = findViewById(R.id.game_title);
+        author = findViewById(R.id.author);
+
+        image.setAnimation(topAnim);
+        title.setAnimation(topAnim);
+        author.setAnimation(topAnim);
+
+
 
         setMainMenuButton();
     }
